5a9d0ebba0a9c0398b003be495c5bb1889332530
Simpanoi-95 <sheillahsimpanoi@gmail.com>
2020-08-23 15:29:17 +0300

    response

--- app/src/main/java/ApiInterface.kt
+++ app/src/main/java/ApiInterface.kt
@@ -1,12 +1,25 @@
 
 
+import ke.co.hello.CoursesResponse
+import ke.co.hello.LoginResponse
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 
 interface ApiInterface {
     @POST("register")
     fun registerStudent(@Body requestBody: RequestBody): Call<RegistrationResponse>
+
+    @POST("login")
+    fun loginStudent(@Body requestBody: RequestBody): Call<LoginResponse>
+
+    @GET("courses")
+    fun getCourses(@Header("Authorization") accessToken: String): Call<CoursesResponse>
+
+
+
 }
