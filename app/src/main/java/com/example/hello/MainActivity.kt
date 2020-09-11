aa05edb71096752c3c57b8a348d3776403b2ca8c
Simpanoi-95 <sheillahsimpanoi@gmail.com>
2020-09-11 14:08:55 +0300

    database

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,10 +1,13 @@
 package ke.co.hello
 
+import ApiInterface
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.MultipartBody
@@ -25,13 +28,27 @@ class MainActivity : AppCompatActivity() {
             btnLOGIN.setOnClickListener {
             val email = etName.text.toString()
             val password = etPassword.text.toString()
+                val error =false
 
-            val requestBody = MultipartBody.Builder()
+                if (email.isBlank() || email.isEmpty()) {
+                    error = true
+                    etUserName.error = "Email is required"
+                }
+
+                if (password.isBlank() || password.isEmpty()) {
+                    error = true
+                    etPassword.error = "Password is required"
+                }
+                 progressBar.max=3000
+                val currentProgress=900
+                ObjectAnimator.ofInt(progressBar,propertyName:"progress",currentProgress)
+
+                val requestBody = MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
                 .addFormDataPart("email", email)
                 .addFormDataPart("password", password)
                 .build()
-
+            
             loginUser(requestBody)
         }
     }
@@ -43,10 +60,12 @@ class MainActivity : AppCompatActivity() {
         loginCall.enqueue(object :Callback<LoginResponse> {
             override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
                 Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
+                progressBar.visibility= View.GONE
             }
 
             override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                 if (response.isSuccessful){
+                    progressBar.visibility= View.GONE
                     Toast.makeText(baseContext, response.body()?.message, Toast.LENGTH_LONG).show()
                     var accessToken = response.body()?.accessToken
                     var sharedPreferences = PreferenceManager.getDefaultSharedPreferences(baseContext)
